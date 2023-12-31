453550e7a0efc796a777a7aadcc59fcd77d03d3f
AnonymouslyFast <calebshearer1@gmail.com>
2023-12-31 16:46:22 -0500

    Added Y to wing converter

--- Projects/Wing_File_Shortener/scripts/generate.js
+++ Projects/Wing_File_Shortener/scripts/generate.js
@@ -8,15 +8,16 @@ function convert(string) {
         if (line[ie] == "x") {
           if (ie == line.length-1) {
             output += ie+1;
+            line[ie] = "-"
           } else {
             output += ie+1 + ",";
           }
         }
+      }
+      if (i+1 != line.length) {
         if (output.charAt(output.length-1) == ",") {
           output = output.slice(0, -1);
         }
-      }
-      if (i+1 != line.length) {
         output += "|";
       }
     }
@@ -26,7 +27,36 @@ function convert(string) {
     return output;
    }
 
+function convertY(string) {
+  var list = string.split("|");
+    if (list[list.length] == '') list[list.length] = null;
+    var output = "";
+    for (var i = 0; i < list.length; i++) {
+      var line = list[i].split(",");
+      for (var ie = 0; ie < line.length; ie++) {
+        if (line[ie] == "Y" || line[ie] == "y") {
+          if (ie == line.length-1) {
+            output += ie+1;
+          } else {
+            output += ie+1 + ",";
+          }
+        }
+      }
+      if (i+1 != line.length) {
+        if (output.charAt(output.length-1) == ",") {
+          output = output.slice(0, -1);
+        }
+        output += "|";
+      }
+    }
+    if (output.charAt(output.length-1) == "|") {
+        output = output.slice(0, -1);
+    }
+    return output;
+}
+
 
 function ChangeValue() {
     document.getElementById('a').value = convert(document.getElementById('ae').value);
+    document.getElementById('aee').value = convertY(document.getElementById('ae').value);
 }
